3e840d8dfe9817c2ab1cdcacfc2769614c24454f
Ryota Arai <ryota.arai@gmail.com>
2013-02-01 00:00:37 +0900

    Initial commit

---
+++ lib/chromium_remote_debugging.rb
@@ -0,0 +1,5 @@
+require "chromium_remote_debugging/version"
+
+module ChromiumRemoteDebugging
+  # Your code goes here...
+end
---
+++ lib/chromium_remote_debugging/version.rb
@@ -0,0 +1,3 @@
+module ChromiumRemoteDebugging
+  VERSION = "0.0.1"
+end
