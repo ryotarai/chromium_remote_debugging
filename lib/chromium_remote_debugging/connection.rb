be0dbe73384e30c154fc963a57d2fcf05d02b2f2
Ryota Arai <ryota.arai@gmail.com>
2013-02-02 00:27:34 +0900

    Implement Connection class.

--- lib/chromium_remote_debugging/connection.rb
+++ lib/chromium_remote_debugging/connection.rb
@@ -1,36 +1,109 @@
 require 'faye/websocket'
-require 'forwardable'
 
 module ChromiumRemoteDebugging
 class Connection
-  extend Forwardable
-
-  # def_delegators :@ws, :onopen=
-  # def_delegators :@ws, :onclose=
-  # def_delegators :@ws, :onmessage=
-  def_delegators :@ws, :send
-
   def initialize(ws_url)
     @ws = ::Faye::WebSocket::Client.new(ws_url)
+
+    set_handers
+
     @request_id = 1
+    @callbacks = {}
+    @callbacks_to_notify = {}
+    @enabled_domains = []
+    @send_buffer = []
+
+    @onopen_handler = nil
+    @onclose_handler = nil
+    @onmessage_handler = nil
+  end
+
+  def set_handers
+    @ws.onopen = lambda do |event|
+      handle_onopen event
+    end
+    @ws.onmessage = lambda do |event|
+      handle_onmessage event
+    end
+    @ws.onclose = lambda do |event|
+      handle_onclose event
+    end
   end
 
   def onopen(&blk)
-    @ws.onopen = blk
+    @onopen_handler = blk
   end
   def onclose(&blk)
-    @ws.onclose = blk
+    @onclose_handler = blk
   end
   def onmessage(&blk)
-    @ws.onmessage = blk
+    @onmessage_handler = blk
+  end
+  def onnotification(method, &blk)
+    @callbacks_to_notify[method] = blk
+
+    domain = parse_method(method)[:domain]
+    enable_notification(domain) unless @enabled_domains.include?(domain)
   end
 
+  def enable_notification(domain)
+    send_command "#{domain}.enable", {} do |result|
+      @enabled_domains << domain unless @enabled_domains.include?(domain)
+    end
+  end
+
+  def parse_method(method)
+    splited = method.split('.')
+    raise "Invalid method name" unless splited.size == 2
+    {domain: splited.first, command: splited.last}
+  end
+
+
+  def handle_onopen(event)
+    @onopen_handler.call(event) if @onopen_handler
+    @send_buffer.each do |content|
+      # @logger.info "send from buffer"
+      send content
+    end
+  end
+  def handle_onclose(event)
+    @onclose_handler.call(event) if @onclose_handler
+  end
+  def handle_onmessage(event)
+    @onmessage_handler.call(event) if @onmessage_handler
+
+    parsed = JSON.parse(event.data)
+
+    if parsed.has_key?('id')
+      # response for command
+      callback = @callbacks[parsed['id']]
+      if parsed['error']
+        callback.call(parsed['error'])
+      else
+        callback.call(parsed['result'] || nil)
+      end
+    else
+      # receive notification
+      callback = @callbacks_to_notify[parsed['method']]
+      callback.call(parsed['params']) if callback
+    end
+  end
+
+
+  def send(content)
+    if @ws.ready_state == ::Faye::WebSocket::API::ReadyStates::CONNECTING
+      @send_buffer << content
+      # @logger.info "added to buffer because the socket is now connecting"
+      return
+    end
+    @ws.send content
+  end
   def send_as_json(obj)
-    @ws.send JSON.generate(obj)
+    send JSON.generate(obj)
   end
-
-  def send_command(method, params)
+  def send_command(method, params, &callback)
     send_as_json("method" => method, "id" => @request_id, "params" => params)
+    @callbacks[@request_id] = callback
     @request_id += 1
   end
 
--- spec/lib/chromium_remote_debugging/connection_spec.rb
+++ spec/lib/chromium_remote_debugging/connection_spec.rb
@@ -2,8 +2,58 @@ require 'spec_helper'
 
 module ChromiumRemoteDebugging
 describe Connection do
-  let(:url) { "ws://localhost:9222/devtools/page/1_1" }
+  include EventedSpec::SpecHelper
+
+  let(:wait) {0.2}
+  let(:port) {9999}
+  let(:url) { "ws://localhost:#{port}/devtools/page/4_1" }
   let(:instance) { described_class.new(url)}
+  let(:server) {DebuggingMockServer.new(port: port)}
+  describe '#onopen' do
+    it "yields with Event object" do
+      open_event = nil
+      em do
+        server.start
+        instance.onopen {|event| open_event = event}
+        done wait
+      end
+      open_event.should be_an_instance_of Faye::WebSocket::Event
+    end
+  end
+  describe '#send' do
+    let(:content) {"content"}
+    it "sends text to server" do
+      em do
+        server.start
+        instance.send content
+        done wait
+      end
+      server.messages.last.should == content
+    end
+  end
+  describe '#send_as_json' do
+    let(:content) {{"this" => "content"}}
+    it "sends a message to #send" do
+      em do
+        instance.should_receive(:send).with('{"this":"content"}')
+        instance.send_as_json content
+        done
+      end
+    end
+  end
+  describe '#send_command' do
+    let(:method) {"method"}
+    let(:params) {{"param1" => "value1"}}
+    it "sends a message to #send_as_json" do
+      em do
+        instance.should_receive(:send_as_json)
+          .with({"method" => method, "id" => 1, "params" => params})
+        instance.send_command method, params
+        done
+      end
+    end
+    it "receives a response to callback"
+  end
 end
 end
 
