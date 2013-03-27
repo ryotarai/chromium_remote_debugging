817a21d28b2dd08886582f4df258e0d2653ea2a7
Ryota Arai <ryota.arai@gmail.com>
2013-03-27 17:23:01 +0900

    Update example.

--- example/simple_example.rb
+++ example/simple_example.rb
@@ -4,18 +4,21 @@ require 'pp'
 client = ChromiumRemoteDebugging::Client.new('localhost', 9222)
 EM.run do
   client.pages.each do |page|
-    page.em_run do |socket|
-      socket.onopen do |event|
+    page.em_run do |conn|
+      conn.onopen do |event|
         puts "onopen"
-        socket.send_command("Page.navigate", "url" => "http://google.com") do |result|
-          p result
+        conn.navigate(url: "http://google.com") do |result|
+#          p result
+          conn.evaluate(expression: "alert('hello');") do |result|
+            p result
+          end
         end
       end
-      socket.onclose do
+      conn.onclose do
         puts "onclose"
       end
-      socket.onnotification "Network.responseReceived" do |params|
-        p params
+      conn.on_response_recieved do |params|
+#        p params
       end
     end
   end
